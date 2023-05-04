e73f5f06f870739c9aef500f233c6eefc51caef6
LeeLeeleCZ <82718819+LeeLeeleCZ@users.noreply.github.com>
2023-05-04 19:50:59 +0200

    Frontend

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="celek center">
+        <h1>File Upload</h1>
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()">
+        <button v-on:click="submitFile()">Submit</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                file: null
+            }
+        },
+        methods: {
+            handleFileUpload() {
+                this.file = this.$refs.file.files[0];
+            },
+            submitFile() {
+                // Create a FormData object
+                const formData = new FormData();
+                formData.append('file', this.file);
+
+                // Make a POST request to the backend
+                axios.post('/api/Print', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+    .celek {
+        width: auto;
+        padding: 30px;
+        background: rgb(128, 128, 128);
+    }
+    .center {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
 </style>
